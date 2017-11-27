30b5fe7ab8d647c621e51d5f09ea2bda87f6df16
Alex Lockwood <alexjlockwood@gmail.com>
2017-11-27 15:47:08 -0800

    remove import

--- app/src/main/java/design/shapeshifter/nestedscrolling/CustomNestedScrollView2.java
+++ app/src/main/java/design/shapeshifter/nestedscrolling/CustomNestedScrollView2.java
@@ -6,7 +6,6 @@ import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 /**
